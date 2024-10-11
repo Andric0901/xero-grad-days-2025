e30510aa8cfe1f61463506870ecb404beeee363c
Andric0901 <75761073+Andric0901@users.noreply.github.com>
2024-10-10 23:53:19 -0400

    Add Invoice class

---
+++ Invoice.py
@@ -0,0 +1,11 @@
+"""..."""
+
+
+class Invoice:
+    def __init__(self, amount: float, tax_rate: float) -> None:
+        self.amount = amount
+        self.tax_rate = tax_rate
+
+
+if __name__ == "__main__":
+    pass
