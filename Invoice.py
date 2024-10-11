8957a9e092be8992657b9c354322b7a99e6b8f7e
Andric0901 <75761073+Andric0901@users.noreply.github.com>
2024-10-11 01:10:13 -0400

    Add more functions for Invoice

--- Invoice.py
+++ Invoice.py
@@ -1,11 +1,31 @@
 """..."""
+import random
+import uuid
 
 
 class Invoice:
     def __init__(self, amount: float, tax_rate: float) -> None:
         self.amount = amount
         self.tax_rate = tax_rate
+        self.uuid = str(uuid.uuid4())
+
+    def __repr__(self):
+        return f"Invoice uuid: {self.uuid}, Amount: {self.amount}, Tax Rate: {self.tax_rate}"
+
+
+def generate_invoices(tax_rate_list: list[float],
+                      repeat: int = 10,
+                      amount: int = 100) -> list[Invoice]:
+    generated_list = []
+    for _ in range(repeat):
+        random_amount = amount
+        random_tax_rate = random.choice(tax_rate_list)
+        new_invoice = Invoice(random_amount, random_tax_rate)
+        generated_list.append(new_invoice)
+    return generated_list
 
 
 if __name__ == "__main__":
-    pass
+    from pprint import pprint
+    invoices = generate_invoices([5, 7, 10, 13])
+    pprint(invoices)
